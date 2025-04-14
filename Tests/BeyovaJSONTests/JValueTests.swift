ba4232582f3104c0d58f50295d852318eed37e76
Canius Chu <canius.chu@outlook.com>
2025-04-14 11:18:18 +0800

    update

--- Tests/BeyovaJSONTests/JValueTests.swift
+++ Tests/BeyovaJSONTests/JValueTests.swift
@@ -18,6 +18,7 @@ struct JValueTests {
     let string = #"{"key1":null,"key2":1,"key3":1.1,"key4":[1,null,"a"]}"#
     #expect(json.debugDescription == string)
   }
+  
   @Test func testDecode() throws {
     let json: JValue = ["key1":nil,"key2":1,"key3":1.1,"key4":[1,nil,"a"]]
     let string = #"{"key1":null,"key2":1,"key3":1.1,"key4":[1,null,"a"]}"#
